16836efbe7ccae4e3e163f2cd8d0c51af7f370dc
Donghyeon Kim <0916dhkim@gmail.com>
2021-07-16 22:09:16 -0400

    Use Docker Swarm Again

--- config.js
+++ config.js
@@ -1,7 +1,13 @@
 const dotenv = require('dotenv')
+const fs = require('fs')
 dotenv.config()
 
+function fromFile(filename) {
+  if (!filename) return undefined
+  return fs.readFileSync(filename, 'ascii')
+}
+
 module.exports = {
-  TOKEN: process.env.TOKEN,
+  TOKEN: fromFile(process.env.TOKEN_FILE) ?? process.env.TOKEN,
   PREFIX: process.env.PREFIX || '!',
 }
